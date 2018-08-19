816b9689eed1b914ee6b6f4210bf8b2a85a415a4
vdwaltc.sci@gmail.com <vdwaltc.sci@gmail.com>
2018-08-19 18:34:17 +0200

    Add otherise and prob_to_cat methods

--- aggregate/batch_aggregate.py
+++ aggregate/batch_aggregate.py
@@ -3,25 +3,25 @@ from typing import List
 import pandas as pd
 
 class BatchAggregate:
-    
+
     def __init__(self):
-        self.col_app = ['weight_', 
-                        'sum_', 
-                        'mode_', 
-                        'mean_', 
-                        'median_', 
-                        'count_', 
-                        'max_', 
+        self.col_app = ['weight_',
+                        'sum_',
+                        'mode_',
+                        'mean_',
+                        'median_',
+                        'count_',
+                        'max_',
                         'min_']
-        self.agg_ops = ['sum', 
-                        'sum', 
-                        lambda x: x.value_counts().index[0] if len(x.value_counts()) else None, 
-                        'mean', 
-                        'median', 
-                        'count', 
-                        'max', 
+        self.agg_ops = ['sum',
+                        'sum',
+                        lambda x: x.value_counts().index[0] if len(x.value_counts()) else None,
+                        'mean',
+                        'median',
+                        'count',
+                        'max',
                         'min']
-        self._weight_cols = []
+        self._weight_cols = []          # list of original columns to be weight summed
         self._sum_cols=[]
         self._mode_cols=[]
         self._mean_cols=[]
@@ -29,32 +29,32 @@ class BatchAggregate:
         self._count_cols=[]
         self._max_cols=[]
         self._min_cols=[]
-        self._weight_var_cols = []
-        self._lst_wv_cols = []
-        self._lst_lst_vars = []
+        self._weight_var_cols = []      # new list of weighted probability columns
+        self._lst_wv_cols = []          # list of all weighted column values
+        self._lst_lst_vars = []         # list of lists of each weighted column's values
         self._df = pd.DataFrame()
         pass
-        
+
 
     def create_aggdict(self):
         """Input a list of columns names, naming the list according to the type of aggregation to be done and
         output dictionary for aggregation and a list of the new column names
-        
+
         Returns:
             dictionary -- dictionary that can be used with with .agg
             list -- new column names of the resultant aggregated dataframe
         """
-        agg_list = [self._weight_cols, 
-                    self._sum_cols, 
-                    self._mode_cols, 
-                    self._mean_cols, 
-                    self._median_cols, 
-                    self._count_cols, 
+        agg_list = [self._weight_cols,
+                    self._sum_cols,
+                    self._mode_cols,
+                    self._mean_cols,
+                    self._median_cols,
+                    self._count_cols,
                     self._max_cols,
                     self._min_cols]
         batch_dict = {}
         col_list = []
-        
+
         for index, aggregation in enumerate(agg_list):
             for col_name in aggregation:
                 if col_name in batch_dict:
@@ -62,61 +62,113 @@ class BatchAggregate:
                 else:
                     batch_dict[col_name] = [self.agg_ops[index]]
                 col_list.append(self.col_app[index] + col_name)
-            
+
         return batch_dict, col_list
 
 
 
-    def weight_catcolumns(self, sort_col, sort_col2=None, linear=True):
+    def weight_catcolumns(self, sort_col, sort_col2=None, linear=True, top_n_max=None):
         """Creat columkns of weights for each varaible in weighted columns
-        
+
         Arguments:
             sort_col {string} -- column name of primary key column which implies order or recency,
                                  such as user_id which is unique, and which also has the order of signup implied.
                                  Date can also be used.
             sort_col2 {string} -- optional column name of secondary key column eg. order_id
-        
+
         Keyword Arguments:
             linear {bool} -- Weight can be linear or exponential (default: {True})
-        
+            top_n {bool} -- Weight can be linear or exponential (default: {True})
+
         Returns:
             dataframe -- new dataframe containing weighted columns
         """
 
-        self._lst_lst_vars = self.list_topn()
-        
+        self._lst_lst_vars = self.list_topn(top_n_max)
+
         if sort_col2:
             sort_by = [sort_col, sort_col2]
         else:
             sort_by = [sort_col]
-            
+
         self._df = self._df.sort_values(by=sort_by).reset_index(drop=True)
         self._df['increment_key'] = self._df.index
         self._df['rank'] = self._df.groupby(sort_col)['increment_key'].rank(method='min')
-        
+
         if linear:
             self._df = self.linear_rank(sort_col)
         else:
             self._df = self.exponential_rank()
-        
+
         del self._df['rank']
         del self._df['increment_key']
-        
+
         return self._df
 
 
-    def list_topn(self):
+    def list_topn(self, top_n_max=None):
         """Creates a list of lists, each list containing the top variables of the weighted columns
-        
+        Arguments:
+            top_n_max {int} -- integer indicating max numbr of unique elements allowed in the column
         Returns:
             List[List] -- List of list of column variables
         """
         top_str = []
         for col in self._weight_cols:
             top_n = list(self._df[col].dropna().unique())
+            if top_n_max:
+                if len(top_n) > top_n_max:
+                    self._df[col] = self.otherise(self._df[col], top_n_max)
+                    top_n = list(self._df[col].dropna().unique())
+                    if "Other" in top_n:
+                        top_n.remove("Other")
             top_str.append(top_n)
-         
+
         return top_str
+
+
+    def prob_to_cat():
+        """Converts columns of probability into a single categorical column
+
+        Returns:
+            dataframe -- dataframe where weighted probability columns have been condensed into a categorical one
+        """
+        top_col_list = []
+        for index, category in enumerate(self._weight_cols):
+            if self._weight_var_cols in self._df.columns[index]:
+                top_col_list.append(self._df.columns[index])
+        drop_columns = []
+        for index, category in enumerate(self._weight_cols):
+            df2 = self._df[top_col_list[index]]
+            drop_columns += top_col_list[index]
+            for i, col_name in enumerate(top_col_list[index]):
+                self._df[str(category)] = df2.idxmax(axis=1)
+            self._df[str(category)] = self._df[str(category)].apply(lambda x: x.split(str(category) + '_', 1)[-1])
+        self._df = self._df.drop(drop_columns, axis=1)
+
+        return self._df
+
+
+    def otherise(self, df_col, n):
+        """Otherise dataframe columns if it contains too many values
+
+        Arguments:
+            df_col {series} -- dataframe column
+            n {int} -- number of maximum values
+
+        Returns:
+            series -- dataframe column with reduced values
+        """
+        z = pd.DataFrame(df_col).copy()
+        z['val'] = 1
+        z.columns = ['key','val']
+        top_n = z.groupby('key').size().sort_values(ascending = False)[:n]
+        top_n = pd.DataFrame(top_n).reset_index()
+        top_n = list(top_n.key.values)
+        df_col = df_col.apply(lambda y: y if y in top_n else "Other")
+        df_col = df_col.fillna("Other")
+
+        return df_col
 
 
     def linear_rank(self, sort_col):
@@ -154,7 +206,7 @@ class BatchAggregate:
             self._weight_var_cols = lst_subcols
 
         return self._weight_var_cols
-            
+
 
 
     @property
@@ -301,4 +353,3 @@ class BatchAggregate:
     def min_cols(self):
         print("deleter of min_cols called")
         self._min_cols = []
-    
